4ff4a455d74fa219e31715e5be8764938ea219ff
PlantsCodes <plantscodes@gmail.com>
2017-04-08 23:05:43 +0800

    REVAMPED CODE
    remade code :D

--- MacDonaldsBreakfast.java
+++ MacDonaldsBreakfast.java
@@ -7,111 +7,110 @@ public class MacDonaldsBreakfast {
 		System.out.println("Buy the following and enjoy! (if possible)");
 		System.out.println("I do not force you to buy anything in any way. This is just for fun. :D");
 		System.out.println(" ");
-
+		
 		Random rand = new Random();
-		int allFoods = rand.nextInt(12) + 1;
-		int addOn = rand.nextInt(2) + 1;
-		int Sides = rand.nextInt(2) + 1;
-		int Drinks = rand.nextInt(2) + 1;
-
+		int mainFood = rand.nextInt(13) + 1;
+		int Beverages = rand.nextInt(9) + 1;
+		int Size = rand.nextInt(2) + 1;
+		
 		System.out.println("Main Course:");
-		switch (allFoods) {
-
+		switch (mainFood) {
+		
 		case 1:
 			System.out.println("Big Breakfast!");
 			break;
-
+			
 		case 2:
 			System.out.println("Breakfast Deluxe!");
 			break;
-
+			
 		case 3:
 			System.out.println("Chicken Muffin!");
 			break;
-
+			
 		case 4:
 			System.out.println("Egg McMuffin!");
 			break;
-
+			
 		case 5:
 			System.out.println("Filet-O-Fish!");
 			break;
-
+			
 		case 6:
 			System.out.println("Hotcakes!");
 			break;
-
+			
 		case 7:
-			System.out.println("Sausage McMuffin!");
-			break;
-
-		case 8:
-			System.out.println("Sunrise Roll! (Sausage)");
-			break;
-
-		case 9:
-			System.out.println("Sunrise Roll! (Chicken Ham)");
-			break;
-
-		case 10:
-			System.out.println("Breakfast Wrap! (Sausage)");
-			break;
-
-		case 11:
-			System.out.println("Breakfast Wrap! (Chicken Ham)");
-			break;
-
-		case 12:
-			System.out.println("Breakfast Wrap! (Chicken Bacon with Spinach)");
+			System.out.println("Hotcakes with Sausage!");
 			break;
 			
-		default:
-			System.err.println("Invalid case! You can report this to @PlantsCodes. D:");
+		case 8:
+			System.out.println("Sausage McMuffin with Egg!");
+			break;
+			
+		case 9:
+			System.out.println("Sausage McMuffin!");
+			break;
+			
+		case 10:
+			System.out.println("Sunrise Roll Sausage!");
+			break;
+			
+		case 11:
+			System.out.println("Sunrise Roll Chicken Ham!");
+			break;
+			
+		case 12:
+			System.out.println("Breakfast Wrap Sausage!");
+			break;
+			
+		case 13:
+			System.out.println("Breakfast Wrap Chicken Ham!");
 			break;
 		}
 		System.out.println(" ");
-
-		if (allFoods == 5) {
-			if (addOn == 1) {
-				System.out.println("Size: Single");
-			} else if (addOn == 2) {
-				System.out.println("Size: Double");
-			}
-		} else if (allFoods == 6) {
-			if (addOn == 1) {
-			} else if (addOn == 2) {
-				System.out.println("(and with Sausage!)");
-				System.out.println(" ");
-			}
-		} else if (addOn == 7) {
-			if (addOn == 1) {
-			} else if (addOn == 2) {
-				System.out.println("(and with Egg!)");
-				System.out.println(" ");
-			}
-		}
-
-		System.out.println("Sides:");
-		if (Sides == 1) {
-			System.out.println("Hashbrowns!");
-		} else if (Sides == 2) {
-			System.out.println("Corn Cup!");
-		}
-		System.out.println(" ");
-
+		
 		System.out.println("Drinks:");
-		if (Drinks == 1) {
-			System.out.println("Iced MILO!");
-		} else if (Drinks == 2) {
+		switch (Beverages) {
+		
+		case 1:
+		case 2:
 			System.out.println("100% Pure Orange Juice!");
-		} else if (Drinks == 3) {
+			break;
+			
+		case 3:
+			System.out.println("Hot MILO!");
+			break;
+			
+		case 4:
 			System.out.println("Hot Tea!");
-		} else if (Drinks == 4) {
-			System.out.println("Cappucinno!");
-		} else if (Drinks == 5) {
-			System.out.println("Premium Roast Coffee!");
-		} else if (Drinks == 6) {
-			System.out.println("Latte!");
+			break;
+			
+		case 5:
+		case 6:
+			System.out.println("Iced MILO!");
+			break;
+			
+		case 7:
+			System.out.println("McCaf\u00E9 Cappuccino!");
+			break;
+			
+		case 8:
+			System.out.println("McCaf\u00E9 Premium Roast Coffee!");
+			break;
+			
+		case 9:
+			System.out.println("McCaf\u00E9 Latte!");
+			break;
+		}
+		if (mainFood == 5) {
+			System.out.println(" ");
+			System.out.println("Size:");
+			if (Size == 1) {
+				System.out.println("Single!");
+			} else if (Size == 2) {
+				System.out.println("Double!");
+			}
 		}
 	}
 }
